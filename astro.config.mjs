561577a819711d353a777c9e856f562e93d81dc4
Denisoed <denisoed@gmal.com>
2024-01-27 12:06:21 +0600

    Added user full info

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
-
 import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind()]
+  integrations: [vue(), tailwind(), icon()],
 });
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,55 +1,58 @@
 <script setup lang="ts">
-import { ref, onMounted, defineProps } from 'vue';
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
-import type { LatLngExpression, Map } from 'leaflet';
+import { ref, onMounted, defineProps } from "vue";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import type { LatLngExpression, Map } from "leaflet";
 
 const props = defineProps({
-	center: {
-		type: Array,
-		required: true
-	}
+  center: {
+    type: Array,
+    required: true,
+  },
 });
 
 const mapRef = ref<HTMLElement | null>(null);
 
 const createMap = (): void => {
-	// Do nothing if the element is not created yet
-	if (!mapRef.value) return;
-	
-	const map = L.map(mapRef.value, {
-		center: props.center,
-		attributionControl: false,
-		zoomControl: false,
-		zoom: 10,
-	});
+  // Do nothing if the element is not created yet
+  if (!mapRef.value) return;
 
-	L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
-		maxZoom: 19,
-	}).addTo(map);
+  const map = L.map(mapRef.value, {
+    center: props.center,
+    attributionControl: false,
+    zoomControl: false,
+    zoom: 10,
+  });
 
-	// Creating the marker
-	createMarker(map, props.center);
+  L.tileLayer(
+    "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
+    {
+      maxZoom: 19,
+    },
+  ).addTo(map);
+
+  // Creating the marker
+  createMarker(map, props.center);
 };
 
 const createMarker = (map: Map): void => {
-	const marker = new L.Marker(props.center);
+  const marker = new L.Marker(props.center);
 
-	marker.addTo(map).openPopup();
+  marker.addTo(map).openPopup();
 };
 
 onMounted(() => {
-	createMap();
+  createMap();
 });
 </script>
 
 <template>
-	<div class="map" ref="mapRef" />
+  <div class="map" ref="mapRef" />
 </template>
 
 <style scoped>
 .map {
-	width: 100%;
-	height: 300px;
+  width: 100%;
+  height: 300px;
 }
 </style>
