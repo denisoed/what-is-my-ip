f656a02e86da6c134c6e2f0654e2a7d4d0bc89bb
Denisoed <denisoed@gmal.com>
2024-01-27 19:18:56 +0600

    Updated user api

--- server/api/info.ts
+++ server/api/info.ts
@@ -1,7 +1,19 @@
-import { publicIpv4 } from 'public-ip';
+function getIPFromRequest(request: any) {
+  return (
+    request.ip ||
+    request.headers['fastly-client-ip'] ||
+    request.headers['x-forwarded-for']?.split(',')[0].trim() ||
+    request.connection.remoteAddress ||
+    request.socket.remoteAddress ||
+    ''
+  );
+}
 
-export default defineEventHandler(async () => {
-  const ip = await publicIpv4();
+export default defineEventHandler(async ({ node }) => {
+  const responseIp = await fetch('https://api.ipify.org/?format=json');
+  const { ip: devIp } = await responseIp.json();  
+  const prodIp = getIPFromRequest(node.req);
+  const ip = process.env.NODE_ENV === 'development' ? devIp : prodIp;
   const response = await fetch(`https://ipinfo.io/${ip}/json`);
   return await response.json();
 });
