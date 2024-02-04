db20b19061abcf2e7d977dee10000c13f59f6d03
Denisoed <denisoed@gmal.com>
2024-02-04 12:44:45 +0600

    Updated json ld

--- pages/index.vue
+++ pages/index.vue
@@ -29,6 +29,28 @@
 <script setup>
 const { data } = useFetch('/api/ip');
 
+const jsonld = {
+  "@context": "http://schema.org",
+  "@type": "WebApplication",
+  "name": "IP Address Lookup Tool",
+  "url": "https://get-info-about.me/",
+  "logo": "https://get-info-about.me/logo.png",
+  "description": "Discover your device's IP address effortlessly with our advanced IP lookup tool. Gain instant access to detailed information, including your city, country, precise location, and time zone, all in one place.",
+  "applicationCategory": "Utilities",
+  "offers": {
+    "@type": "Offer",
+    "price": "0.00",
+    "priceCurrency": "USD",
+    "availability": "http://schema.org/InStock"
+  },
+  "browserRequirements": "Requires any modern web browser",
+  "operatingSystem": "All",
+  "softwareVersion": "1.0",
+  "featureList": "IP Address Detection, Geolocation Information, Real-time Analysis",
+  "screenshot": "https://get-info-about.me/screenshot.jpg",
+  "accessibilityAPI": "ARIA"
+}
+
 useHead({
   title: `What's My IP? Discover Your IP Address, Location, and More`,
   meta: [
@@ -44,27 +66,7 @@ useHead({
   script: [
     {
       type: 'application/ld+json',
-      json: {
-        "@context": "http://schema.org",
-        "@type": "WebApplication",
-        "name": "IP Address Lookup Tool",
-        "url": "https://get-info-about.me/",
-        "logo": "https://get-info-about.me/logo.svg",
-        "description": "Discover your device's IP address effortlessly with our advanced IP lookup tool. Gain instant access to detailed information, including your city, country, precise location, and time zone, all in one place.",
-        "applicationCategory": "Utilities",
-        "offers": {
-          "@type": "Offer",
-          "price": "0.00",
-          "priceCurrency": "USD",
-          "availability": "http://schema.org/InStock"
-        },
-        "browserRequirements": "Requires any modern web browser",
-        "operatingSystem": "All",
-        "softwareVersion": "1.0",
-        "featureList": "IP Address Detection, Geolocation Information, Real-time Analysis",
-        "screenshot": "https://get-info-about.me/screenshot.jpg",
-        "accessibilityAPI": "ARIA"
-      }
+      innerHTML: jsonld
     }
   ]
 });
